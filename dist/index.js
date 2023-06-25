319c8da950d9dcb63a11bbb1ce4e50a7e80ddaec
lctrayden25 <79618253+lctrayden25@users.noreply.github.com>
2023-06-25 15:15:53 +0800

    test pagination

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const mockData_1 = require("./mock/mockData");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use((0, cors_1.default)(corsOptions));
 app.get("/", (req, res) => {
-    return res.json("tes");
+    return res.json("home");
+});
+app.get("/team-list", (req, res) => {
+    const { page, limit } = req.query;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const list = mockData_1.defaultData.slice(startIndex, endIndex);
+    return res.json(list);
+});
+app.get("/team-count", (req, res) => {
+    const list = mockData_1.defaultData.length;
+    return res.json(list);
 });
 app.listen(9000, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${9000}`);
--- index.ts
+++ index.ts
@@ -1,14 +1,41 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { defaultData, Person } from "./mock/mockData";
 
 dotenv.config();
 
 const app: Express = express();
 
-app.get("/", (req, res) => {
-    return res.json("tes")
-})
+const corsOptions = {
+	origin: ["http://localhost:3000"],
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req: Request, res: Response) => {
+	return res.json("home");
+});
+
+app.get("/team-list", (req: Request, res: Response) => {
+	const { page, limit } = req.query as any;
+
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+
+	const list = defaultData.slice(startIndex, endIndex);
+
+	return res.json(list);
+});
+
+app.get("/team-count", (req: Request, res: Response) => {
+	const list = defaultData.length;
+
+	return res.json(list);
+});
 
 app.listen(9000, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${9000}`);
-  });
+	console.log(`⚡️[server]: Server is running at http://localhost:${9000}`);
+});
