f89e733e159e60a182e68a5447ae1a4761cdce10
lctrayden25 <79618253+lctrayden25@users.noreply.github.com>
2023-11-11 01:27:06 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -28,6 +28,7 @@ app.use((0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.static('public'));
 app.get("/", (req, res) => {
     return res.json("Homepage");
 });
--- index.ts
+++ index.ts
@@ -27,6 +27,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'))
 
 app.get("/", (req: Request, res: Response) => {
 	return res.json("Homepage");
